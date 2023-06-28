ccf4862d8a7fa814614d609403260f3657032cbc
Jim Telles <tellesj@oregonstate.edu>
2023-06-28 11:10:07 -0700

    7th

--- hello_gradescope.py
+++ hello_gradescope.py
@@ -12,9 +12,4 @@ def gradescope_test():
     return 42
 
 
-cur_version = sys.version_info
-
-if cur_version >= (3,7):
-    print("This is an acceptable version of Python, version " + str(cur_version[0]) + '.' + str(cur_version[1]) + '.')
-else:
-    print("Your Python version is too low, it needs to be 3.7 or greater and this is " + str(cur_version[0]) + '.' + str(cur_version[1]) + '.')
+# sys.version_info
