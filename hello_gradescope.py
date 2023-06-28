77423b15627cd0d9aa3395c5e75b2a93ad9fa076
Jim Telles <tellesj@oregonstate.edu>
2023-06-28 10:34:08 -0700

    Initial commit

---
+++ hello_gradescope.py
@@ -0,0 +1,11 @@
+# Name: Jim Telles
+# OSU Email: tellesj@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: Gradescope check
+# Due Date: 7.3.23
+# Description: Checking to see if I have a sufficient version of Python installed as well as a connection to the
+#               gradescope assignment checker. And seeing if I can pull it from github.
+
+
+def gradescope_test():
+    return 42
